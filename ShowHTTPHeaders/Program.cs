fba419f8f771edf97da1d638a4ce10a21787a0fb
RickLeinecker <Rick@JSVentures.com>
2019-07-15 17:47:55 -0400

    Add files via upload

---
+++ ShowHTTPHeaders/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ShowHTTPHeaders
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			if( args.Length < 1 )
+			{
+				Console.WriteLine("You need to specify at least one URL.");
+				return;
+			}
+
+			foreach (string s in args)
+			{
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(s);
+				request.Method = "GET";
+				HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+				Console.WriteLine(response.Headers);
+			}
+
+		}
+	}
+}
